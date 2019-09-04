360d500229423cffde1674c7e8cfa73fa36a00bc
Phyllis Kan <pyk9q7@gmail.com>
2019-09-04 04:41:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,35 +1,35 @@
 # code here!
 class School
   attr_accessor :name, :roster
-  
+
   def initialize(name)
     @name = name
     @roster = {}
-  end 
-  
+  end
+
   def add_student(student, level)
     # this sort of means #=> a || a = b ... or #=> x || x = y
     # it's freak'n weird
     roster[level] ||= []
     roster[level] << student
-  end 
-  
+  end
+
   def grade(level)
-    roster.detect do |x, y| 
+    roster.detect do |x, y|
       if x == level
-        return y 
-      end 
-    end 
-  end 
-  
-  def sort 
+        return y
+      end
+    end
+  end
+
+  def sort
     new_hash = {}
-    roster.each do |x, y| 
-      new_hash[x] = y.sort 
-    end 
+    roster.each do |x, y|
+      new_hash[x] = y.sort
+    end
     new_hash
-  end   
-end 
+  end
+end
 
 
 
